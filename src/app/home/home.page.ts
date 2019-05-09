b204bd30578f20b3e88c467fa333266631e33324
LEONARDO SOUZA DE FARIAS DA SILVA <540326665@alunos.etec.itaqua>
2019-05-08 22:32:12 -0300

    colocamos o banco pra funcionar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  //ele é iniciado toda vez que entre na pagina
-  viewDidEnter(){
-    const tamanhoBanco = sessionStorage.length
-    //let é a criacao de variavel em tapscript
-    for (let index = 0; index < sessionStorage.length; index++) {
-      const element = sessionStorage[index];
-      
+    //variavel é um array
+    planejamentos = []
+    //ele é iniciado toda vez que entre na pagina
+    ionViewDidEnter() {
+        //zera o array
+        this.planejamentos = []
+        //define o tamanho do banco
+        const tamanhoBanco = sessionStorage.length
+        //let é a criacao de variavel em tapscript
+        for (let index = 0; index < tamanhoBanco; index++) {
+            const chave = sessionStorage.key(index)
+            if(chave !== 'ionic-persist-config') {
+                const planejamento = sessionStorage.getItem(chave)
+                this.planejamentos.push(JSON.parse(planejamento))
+            }
+        }
     }
-  }
 }
